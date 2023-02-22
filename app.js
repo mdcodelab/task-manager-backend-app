53a0356cebd815e977b104015194e9bb5c4f5951
miha11-hash <d_mihaela@msn.com>
2023-02-22 11:18:03 +0200

    custon error handler

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
 const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 
 //middleware
@@ -15,6 +16,7 @@ const port=3000;
 app.use('/api/v1/tasks', tasks)
 
 app.use(notFound)
+app.use(errorHandlerMiddleware)
 
 
 
